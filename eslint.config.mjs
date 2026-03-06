48ec20226a30cc431d646cbb6bcae2acf7288fa0
tj-commits <thefoxjones@gmail.com>
2026-03-06 16:28:32 -0600

    use TS-compatible ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,19 @@
 import config from "@10xly/eslint-config"
+import tseslint from "typescript-eslint"
 
-export default config
+export default [
+  ...config,
+  ...tseslint.configs.all.map(c => ({
+    ...c,
+    files: ["**/*.ts", "**/*.tsx"],
+  })),
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    }
+  },
+]
